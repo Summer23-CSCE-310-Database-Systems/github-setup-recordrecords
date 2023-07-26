60b95d5c8417c5edba39887cbc125c4055abe024
Carson23 <carson.lee23@gmail.com>
2023-07-26 16:40:06 -0500

    App.py connected with DB
    added 2 functions for connecting and retrieving data. Added sample data to search-cus file.

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, jsonify
+import psycopg2
 app = Flask(__name__)
+cursor = None
+def connect_db():
+    db_link = 'postgres://mzlmdawh:68uKpdQLKb5UwzZces5mCIbhtG2yOH3o@batyr.db.elephantsql.com/mzlmdawh'
+
+    conn = psycopg2.connect(db_link)
+    return conn
+
+@app.route('/get_data', methods=['GET'])
+def get_table(table):
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        # Replace 'your_table_name' with the name of the table you want to query
+        query = f"SELECT * FROM {table};"
+        print('executing ', query)
+        cursor.execute(query)
+        # Fetch all data from the query result
+        data = cursor.fetchall()
+        # Close the cursor and database connection
+        cursor.close()
+        conn.close()
+        # Convert data to a JSON response
+        return data
+
+    except Exception as e:
+        print("ERROR")
+        return jsonify({'error': str(e)})
 
 @app.route('/')
 def index():
@@ -12,7 +39,9 @@ def franchise():
 
 @app.route('/search-cus')
 def search_cus():
-    return render_template('search-cus.html')
+    data = get_table('vinyls')
+    print('data',data)
+    return render_template('search-cus.html', data=data)
 
 
 if __name__ == "__main__": 
