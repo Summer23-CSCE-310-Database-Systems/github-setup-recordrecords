c707d4c0d6ec72438b22fae9c53369f13a91214b
Carson23 <carson.lee23@gmail.com>
2023-07-28 18:56:56 -0500

    Squashed commit of the following:
    commit 48aaed0a53925b0404c5584531e0d9dbc7b59530
    Author: Carson23 <carson.lee23@gmail.com>
    Date:   Fri Jul 28 18:56:26 2023 -0500
    
        s://github.com/Summer23-CSCE-310-Database-Systems/github-setup-recordrecords
    
    commit 53286bf134b2ffc2318dbd09bd77e97181774835
    Author: Carson23 <carson.lee23@gmail.com>
    Date:   Wed Jul 26 16:40:06 2023 -0500
    
        App.py connected with DB
    
        added 2 functions for connecting and retrieving data. Added sample data to search-cus file.

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, render_template, url_for, jsonify
 import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mzlmdawh:68uKpdQLKb5UwzZces5mCIbhtG2yOH3o@batyr.db.elephantsql.com/mzlmdawh'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 """
     SETUP:
@@ -20,23 +15,34 @@ migrate = Migrate(app, db)
             flask db migrate
             flask db upgrade
 """
-class ManagerModel(db.Model):
-    __tablename__ = 'managers'
 
-    mana_id = db.Column(db.Integer, primary_key=True)
-    mana_fname = db.Column(db.String())
-    mana_lname = db.Column(db.String())
-    mana_phone = db.Column(db.String())
+cursor = None
+def connect_db():
+    db_link = 'postgres://mzlmdawh:68uKpdQLKb5UwzZces5mCIbhtG2yOH3o@batyr.db.elephantsql.com/mzlmdawh'
 
-    def __init__(self, id, fname, lname, phone):
-        self.mana_id = id
-        self.mana_fname = fname
-        self.mana_lname = lname
-        self.mana_phone = phone
+    conn = psycopg2.connect(db_link)
+    return conn
 
-    def __repr__(self):
-        return f'<Manager {mana_id}>'
+@app.route('/get_data', methods=['GET'])
+def get_table(table):
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        # Replace 'your_table_name' with the name of the table you want to query
+        query = f"SELECT * FROM {table};"
+        print('executing ', query)
+        cursor.execute(query)
+        # Fetch all data from the query result
+        data = cursor.fetchall()
+        # Close the cursor and database connection
+        cursor.close()
+        conn.close()
+        # Convert data to a JSON response
+        return data
 
+    except Exception as e:
+        print("ERROR")
+        return jsonify({'error': str(e)})
 
 @app.route('/')
 def index():
@@ -48,7 +54,9 @@ def franchise():
 
 @app.route('/search-cus')
 def search_cus():
-    return render_template('search-cus.html')
+    data = get_table('vinyls')
+    print('data',data)
+    return render_template('search-cus.html', data=data)
 
 
 if __name__ == "__main__": 
